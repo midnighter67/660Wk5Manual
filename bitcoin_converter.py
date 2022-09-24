a6f9eaf4b663c5ccd908c95742225b6032bfd016
midnighter67 <101069922+midnighter67@users.noreply.github.com>
2022-09-23 21:35:01 -0600

    Add files via upload

---
+++ bitcoin_converter.py
@@ -0,0 +1,16 @@
+# Mike Wooldridge
+# SWDV 600 2W 21/FA1
+# Week 1 Review Programming Exercise 2
+# Bitcoin Converter
+#   Convert bit coin to US dollars
+# Sample output:
+#   As of 8/1/17 at 11:13 am, bitcoin is currently trading at $2086 per bitcoin.
+#   Enter the bitcoin amount: .5
+#   That is worth 1043 us dollars.
+# 8/26/21 at 2:20pm $46,953.29 per bitcoin
+bitcoinPricePerUnit = 47953
+print("As of 8/26/21 at 2:20pm, bitcoin is currently trading at $47953 per bitcoin")
+bitcoinInput = input("Enter the bitcoin amount:  ")
+bitcoins = float(bitcoinInput)
+bitcoinValue = round(bitcoins * bitcoinPricePerUnit)
+print("That is worth " + str(bitcoinValue) + " US dollars.")
